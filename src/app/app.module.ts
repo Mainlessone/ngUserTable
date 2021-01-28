1841aafd28880f66efd6db9305c55e8c69852d43
Vyacheslav Yastrebtsev <syi.nrgy@gmail.com>
2021-01-28 10:38:04 +0200

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
